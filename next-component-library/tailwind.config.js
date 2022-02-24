ac43e76d34bfca59e52fd403bb80ffee07145cf5
Hannah Laek <hannahlaek@Hannahs-MacBook-Pro.local>
2022-02-24 11:59:25 -0500

    start helper function for tailwind

--- next-component-library/tailwind.config.js
+++ next-component-library/tailwind.config.js
@@ -1,3 +1,4 @@
+const convertPixelsToRem = require('./utils/helper');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require("path");
 
@@ -5,6 +6,13 @@ module.exports = {
   content: [path.join(__dirname, "./src/**/*.(js|jsx|ts|tsx)")],
   theme: {
     extend: {},
+    spacing: {
+      '4': convertPixelsToRem(4, 16),
+      '2': '12px',
+      '3': '16px',
+      '5': '32px',
+      '6': '48px',
+    },
   },
   plugins: [],
 };
--- next-component-library/utils/helper.ts
+++ next-component-library/utils/helper.ts
@@ -7,3 +7,9 @@ export interface ErrorResponse {
 export function instanceOfErrorResponse(err: unknown): err is ErrorResponse {
   return true;
 }
+
+function convertPixelsToRem(pixel: number, basefont: number) {
+  const rem = pixel / basefont;
+  return `${rem}rem`;
+}
+export default convertPixelsToRem;
