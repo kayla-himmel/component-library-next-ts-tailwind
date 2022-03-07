79d2ee01b47b782bfc543a20277ea2a7cb5f76d7
Kayla Himmelberger <38771553+kaylahimmel@users.noreply.github.com>
2022-03-07 12:36:24 -0500

    pulled main in from origin

---
+++ next-component-library/src/components/Dropdown/Dropdown.stories.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ComponentStory, Meta } from '@storybook/react';
+import Dropdown from './Dropdown';
+import { DropdownData } from './Dropdown.interfaces';
+
+export default {
+  title: 'Components/Dropdown',
+  component: Dropdown,
+} as Meta;
+
+const sampleDropdownData: DropdownData[] = [
+  { id: '0', label: 'Istanbul, TR (AHL)' },
+  { id: '1', label: 'Paris, FR (CDG)' },
+  { id: '2', label: 'Columbus, OH (CMH)' },
+];
+
+const Template: ComponentStory<typeof Dropdown> = (args) => <Dropdown {...args} />;
+
+// Dropdown example in Storybook
+export const DropdownPrimary = Template.bind({});
+DropdownPrimary.args = {
+  title: 'Select an Option',
+  data: sampleDropdownData,
+};
